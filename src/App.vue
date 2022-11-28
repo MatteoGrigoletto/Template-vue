5b39f44d2e78cce3b2e390ca74da5ffe327b8da6
MatteoGrigoletto <mttgrigoletto@gmail.com>
2022-11-28 18:10:31 +0100

    Aggiunge modifiche struttura

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,30 @@
-<script>
-import PageTitle from './components/PageTitle.vue';
-
-export default {
-  components: {
-    PageTitle
-  }
-}
-</script>
-
 <template>
-  <div>
-    <PageTitle/>
-  </div>
+
+<AppHeader/>
+<AppMain/>
+<AppFooter/>
+
 </template>
 
-<style scoped>
+<script>
+import AppHeader from './components/AppHeader.vue';
+import AppMain from './components/AppMain.vue';
+import AppFooter from './components/AppFooter.vue';
+  export default {
+    data(){
+      return{
+
+      }
+    },
+    components:{
+      AppHeader,
+      AppMain,
+      AppFooter,
+
+    }
+  }
+</script>
+
+<style lang="scss" scoped>
+@import './style/global-scss.scss';
 </style>
